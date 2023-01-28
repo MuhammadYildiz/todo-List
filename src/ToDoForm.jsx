ee49f33df078c66d7d62fd4b05ec3d6aaba64f32
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-28 13:29:15 +0100

    Update ToDoForm.jsx

--- src/ToDoForm.jsx
+++ src/ToDoForm.jsx
@@ -18,14 +18,14 @@ export default function ToDoForm(props) {
     }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input className='border-2 border-black outline-none px-2 p-1 m-2'
+            <form onSubmit={handleSubmit} className="flex flex-col sm:w-[50%] lg:w-[30%] m-auto ">
+                <input className='border-2 border-black outline-none px-2 mr-2 p-1 '
                     type="text"
                     placeholder='Add a todo....'
                     value={input}
                     onChange={getInput}
                 />
-                <button type='submit' className='border-2 border-black outline-none px-2 p-1 m-2 font-bold hover:bg-green-900 hover:text-white' >
+                <button type='submit' className='border-2 border-black outline-none px-2 p-1 m-auto my-2 font-bold hover:bg-green-900 hover:text-white max-w-[40%] min-w-[30%]  ' >
                     Add
                 </button>
             </form>
