3abf1154d3b0d95ef2760ae6f49ab5944ef1c04f
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-28 13:29:20 +0100

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -8,7 +8,7 @@ export default function TodoItem(props) {
                         type='button' onClick={() => props.doneTodo(props.todo.id)}>
                         {props.todo.done ? "Undo" : "Done"}
                     </button>
-                    <p className={props.todo.done ? "line-through border-2 border-red-500 outline-none px-2 p-1 m-2 min-w-[50%] " : 'border-2 border-black outline-none px-2 p-1 m-2 min-w-[50%]'}>
+                    <p className={props.todo.done ? "line-through border-2 border-red-500 outline-none px-2 p-1 m-2 min-w-[50%] " : 'border-2 border-black outline-none px-2 p-1 m-2 min-w-[50%] '}>
                         {props.todo.text}
                     </p>
                     <button className='border-2 border-black outline-none px-2 p-1 m-2 hover:bg-red-800 hover:text-white'
