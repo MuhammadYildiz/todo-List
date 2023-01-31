ed670cea4483a491e24df12904029adfe5b94036
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-31 14:34:47 +0100

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -3,15 +3,15 @@ export default function TodoItem(props) {
     return (
         <div>
             <ul>
-                <li className="flex justify-around  w-full sm:w-[60%] m-auto">
-                    <button className='border-2 border-black outline-none px-2 p-1 m-2 hover:bg-yellow-600 hover:text-white'
+                <li className="flex justify-center w-full sm:w-[60%] m-auto">
+                    <button className='border-2 border-black outline-none px-2 p-1 my-2 hover:bg-yellow-600 hover:text-white'
                         type='button' onClick={() => props.doneTodo(props.todo.id)}>
                         {props.todo.done ? "Undo" : "Done"}
                     </button>
-                    <p className={props.todo.done ? "line-through border-2 border-red-500 outline-none px-2 p-1 m-2 min-w-[50%] " : 'border-2 border-black outline-none px-2 p-1 m-2 min-w-[50%] '}>
+                    <p className={props.todo.done ? "line-through border-2 border-red-500 outline-none px-2 p-1 m-2 w-[50%] overflow-scroll " : 'border-2 border-black outline-none px-2 p-1 m-2 w-[50%] overflow-scroll '}>
                         {props.todo.text}
                     </p>
-                    <button className='border-2 border-black outline-none px-2 p-1 m-2 hover:bg-red-800 hover:text-white'
+                    <button className='border-2 border-black outline-none px-2 p-1 my-2 hover:bg-red-800 hover:text-white'
                         type='button' onClick={() => props.removeTodo(props.todo.id)}>
                         Delete
                     </button>
